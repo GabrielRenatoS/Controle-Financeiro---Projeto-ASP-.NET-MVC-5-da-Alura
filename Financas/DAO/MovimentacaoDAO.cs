6b577534635a0d55d98b9309ad6b4ac5f721e12f
GabrielRenatoS <grenatosilva@gmail.com>
2021-08-04 17:57:52 -0300

    Adicionar arquivos de projeto.

---
+++ Financas/DAO/MovimentacaoDAO.cs
@@ -0,0 +1,66 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Financas.DAO
+{
+    public class MovimentacaoDAO
+    {
+        private FinancasContext context;
+
+        public MovimentacaoDAO(FinancasContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adiciona (Movimentacao movimentacao)
+        {
+            context.Movimentacoes.Add(movimentacao);            
+            context.SaveChanges();
+        }
+
+        public IList<Movimentacao> Lista()
+        {
+            return context.Movimentacoes.ToList();
+        }
+
+        public IList<Movimentacao> BuscaPorUsuario(int? usuarioId)
+        {
+            return context.Movimentacoes.Where(m => m.UsuarioId == usuarioId).ToList();
+        }
+
+        internal IList<Movimentacao> Busca(decimal? valorMinimo, decimal? valorMaximo, 
+                                            DateTime? dataMinima, DateTime? dataMaxima, 
+                                            Tipo? tipo, int? usuarioId)
+        {
+            IQueryable<Movimentacao> busca = context.Movimentacoes;
+
+            if (valorMinimo.HasValue)
+            {
+                busca.Where(m => m.Valor >= valorMinimo);
+            }
+            if (valorMaximo.HasValue)
+            {
+                busca.Where(m => m.Valor <= valorMaximo);
+            }
+            if (dataMinima.HasValue)
+            {
+                busca.Where(m => m.Data >= dataMinima);
+            }
+            if (dataMaxima.HasValue)
+            {
+                busca.Where(m => m.Data <= dataMaxima);
+            }
+            if (tipo.HasValue)
+            {
+                busca.Where(m => m.Tipo == tipo);
+            }
+            if (usuarioId.HasValue)
+            {
+                busca.Where(m => m.Id == usuarioId);
+            }
+            return busca.ToList();
+        }
+    }
+}
---
+++ Financas/DAO/UsuarioDAO.cs
@@ -0,0 +1,33 @@
+﻿using Financas.Entidades;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Financas.DAO
+{
+    public class UsuarioDAO 
+    {
+        private FinancasContext context;
+
+        public UsuarioDAO(FinancasContext context)
+        {
+            this.context = context;
+        }
+
+        public void Adiciona(Usuario usuario)
+        {
+            context.Usuarios.Add(usuario);
+            context.SaveChanges();
+        }
+
+        public void Remove(Usuario usuario)
+        {
+            context.Entry(usuario).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public IList<Usuario> Lista() {
+            return context.Usuarios.ToList();
+        }
+    }
+}
